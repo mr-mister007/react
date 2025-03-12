59c7837243502f41e13b605d68d525e68da4fcff
ARAVIND M. V <48949823+mr-mister007@users.noreply.github.com>
2025-03-12 10:28:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Pull the Docker image tarball from GitHub Actions artifact
-curl -L -o docker-image.tar "https://github.com/mr-mister007/react/releases/download/latest/docker-image.tar"
+curl -L -o docker-122image.tar "https://github.com/mr-mister007/react/releases/download/latest/docker-image.tar"
 
 # Load the Docker image
 docker load -i docker-image.tar
 
 # Stop and remove the existing container if it exists
 
-docker stop my-react-container || true
+docker stop my-react-container122 || true
 docker rm your-react-container || false
 
 # Run the new container
-docker run -d --name my-new-react-container -p 80:80 your-react-app
+docker run -d --name my-new-react-container -p 80:80 your-react-appliction
